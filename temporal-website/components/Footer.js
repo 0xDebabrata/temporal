3dad609da634e5b4ff542b0e19f2c1d2757f4a37
Debabrata Mondal <debabrata.js@protonmail.com>
2022-06-24 21:21:35 +0530

    update website footer to include logos

--- temporal-website/components/Footer.js
+++ temporal-website/components/Footer.js
@@ -1,16 +1,36 @@
 export default function Footer() {
   return (
     <div className="flex content-end justify-center h-10 bg-zinc-700">
-      <p className="mt-2 text-base align-middle text-light-blue font-Montserrat">
+      <p className="flex items-center justify-center text-base align-middle text-light-blue font-Montserrat">
+        <img
+          width="24px"
+          height="24px"
+          src="https://implicit-orange-booby.faviconkit.com/linode.com/256"
+          alt="Linoce favicon"
+          className="mr-2 rounded-md"
+        />
         <a href="https://www.linode.com/"
           target="_blank"
           rel="noopener"
-        >Linode </a>
-        x
+          className="hover:underline hover:underline-offset-4"
+        >
+          Linode
+        </a>
+        <img
+          width="24px"
+          height="24px"
+          src="https://implicit-orange-booby.faviconkit.com/hashnode.com/256"
+          alt="Linoce favicon"
+          className="mx-2 rounded-md"
+        />
         <a href="https://www.hashnode.com/"
           target="_blank"
           rel="noopener"
-        > Hashnode</a> hackathon
+          className="mr-1 hover:underline hover:underline-offset-4"
+        > 
+          Hashnode
+        </a>
+        hackathon
       </p>
     </div>
   )
